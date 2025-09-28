cc5b693bf1ee27458ea434248dfe0d5db678ac4f
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2025-09-29 00:33:45 +0700

    update favicon

--- brain-vil/app/layout.js
+++ brain-vil/app/layout.js
@@ -3,14 +3,19 @@ import '../styles/global.css';
 
 
 export const metadata = {
-title: 'CASTICaD',
+  title: "CASTICaD",
+  description: "Cross-Attentional SpatioTemporal Brain Connectivity Study",
 };
 
-
 export default function RootLayout({ children }) {
-return (
-  <html lang="en">
-  <body>{children}</body>
-  </html>
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/brain-line-icon.svg" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
